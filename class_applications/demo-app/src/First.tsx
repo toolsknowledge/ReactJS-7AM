d05fdac2a1e81d3e1feea9825c521f066bd323f8
samba.pendela@gmail.com <samba.pendela@gmail.com>
2020-12-21 08:01:02 +0530

    functional components

---
+++ class_applications/demo-app/src/First.tsx
@@ -0,0 +1,60 @@
+//import React class
+//React class provides the Component environment
+//React class available in "react" package
+import React from "react";
+
+
+/*
+    //create the function
+    function First(){
+        return(<h1>Welcome to React Stateless Component !!!</h1>)
+    };
+
+    //export the Component
+    export default First;
+*/
+
+
+/*
+    export default function First(){
+        return(<React.Fragment>
+            <h1>React !!!</h1>
+            <h1>Encapsulation !!!</h1>
+        </React.Fragment>);  
+    };
+*/
+
+
+
+//export const First = ()=>(<h1>Welcome to React !!!</h1>)
+
+
+/*
+    export const First = ()=>(<React.Fragment>
+        <h1>Hello_1</h1>
+        <h2>Hello_2</h2>
+    </React.Fragment>)
+*/
+
+
+export const First = ()=>{
+    return(<React.Fragment>
+        <h1>Hello_1</h1>
+        <h1>Hello_2</h1>
+    </React.Fragment>)
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
