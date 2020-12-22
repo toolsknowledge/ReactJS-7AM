687f9b88b11be2134cfd875024ac2f5ddc243e92
samba.pendela@gmail.com <samba.pendela@gmail.com>
2020-12-22 08:57:43 +0530

    code added

--- class_applications/first-app/src/App.tsx
+++ class_applications/first-app/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-       <h1>Welcome to ReactJS With TypeScript....!</h1>
+const App = (props:any)=>{
+  return(
+    <div>
+      Hello
     </div>
-  );
+    <div>
+      Welcome
+    </div>
+  )
 }
-
 export default App;
